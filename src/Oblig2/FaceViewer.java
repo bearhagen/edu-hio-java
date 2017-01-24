77397f6b37e57b5bb81c037045bd82715ab14e34
Bear Hagen <post@bearhagen.com>
2017-01-24 22:37:56 +0100

    Task 1 - draw a face

---
+++ src/Oblig2/FaceViewer.java
@@ -0,0 +1,42 @@
+package Oblig2;
+
+import javax.swing.JFrame;
+import javax.swing.JComponent;
+import javax.swing.WindowConstants;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.BasicStroke;
+
+/**
+ * Created by bjorn on 1/24/2017.
+ */
+public class FaceViewer {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.add(new FaceComponent());
+        frame.setLocationRelativeTo(null);  // Center the frame on the screen
+        frame.setVisible(true);             // Show the frame
+    }
+
+    private static class FaceComponent extends JComponent {
+        @Override
+        protected void paintComponent(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setStroke(new BasicStroke(5)); // To get some width on the lines
+            g.setColor(new Color(0, 0, 0));
+
+            // Face
+            g.drawOval(75, 50, 150, 150);
+
+            // Eyes
+            g.fillOval(125, 100, 10, 10);
+            g.fillOval(155, 100, 10, 10);
+
+            // Mouth
+            g.drawArc(125, 125, 40, 20, 180, 180);
+        }
+    }
+}
